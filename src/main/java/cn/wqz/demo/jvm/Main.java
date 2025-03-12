de0673ae9621ce5ec518e19639a0f65157801aee
buptwqz <879680229@qq.com>
2025-03-12 20:23:05 +0800

    命令行工具

---
+++ src/main/java/cn/wqz/demo/jvm/Main.java
@@ -0,0 +1,27 @@
+package cn.wqz.demo.jvm;
+
+/**
+ * Author: Qizheng Wang
+ * Email:  879680229@qq.com
+ * Date:   2025/3/12 下午7:08
+ * GitHub: https://github.com/buptwqz
+ * Description:
+ **/
+public class Main {
+    public static void main(String[] args) {
+        Cmd cmd = Cmd.parse(args);
+        if (!cmd.ok || cmd.helpFlag) {
+            System.out.println("Usage: <main class> [-options] class [args...]");
+            return;
+        }
+        if (cmd.versionFlag) {
+            System.out.println("java version \"1.8.0\"");
+            return;
+        }
+        startJVM(cmd);
+    }
+
+    private static void startJVM(Cmd cmd) {
+        System.out.printf("classpath:%s class:%s args:%s\n", cmd.classpath, cmd.getMainClass(), cmd.getAppArgs());
+    }
+}
